b37f12b5266733e63751e817e1e98cc3c1e16f82
mark.feith <mark.feith@shiwaforce.com>
2019-03-01 11:52:23 +0100

    ng-show/hide, ng-click practise

--- app/app.js
+++ app/app.js
@@ -1,29 +1,40 @@
 var myNinjaApp = angular.module("myNinjaApp", []);
 
-myNinjaApp.controller("NinjaController", [
-   "$scope",
-   function($scope) {
-      $scope.ninjas = [
-         {
-            name: "Yoshi",
-            belt: "green",
-            rate: 50
-         },
-         {
-            name: "Crystal",
-            belt: "yellow",
-            rate: 30
-         },
-         {
-            name: "Ryu",
-            belt: "orange",
-            rate: 10
-         },
-         {
-            name: "Shaun",
-            belt: "black",
-            rate: 1000
-         }
-      ];
+myNinjaApp.controller("NinjaController", ["$scope", function ($scope) {
+
+   $scope.removeNinja = function (ninja) {
+      var removedNinja = $scope.ninjas.indexOf(ninja);
+      $scope.ninjas.splice(removedNinja, 1);
    }
+
+   $scope.ninjas = [
+      {
+         name: "Yoshi",
+         belt: "green",
+         rate: 50,
+         available: true
+      },
+
+      {
+         name: "Crystal",
+         belt: "yellow",
+         rate: 30,
+         available: true
+      },
+
+      {
+         name: "Ryu",
+         belt: "orange",
+         rate: 10,
+         available: false
+      },
+
+      {
+         name: "Shaun",
+         belt: "black",
+         rate: 1000,
+         available: true
+      }
+   ];
+}
 ]);
